948e3384a55e370182ac248886cea9fc2897e20d
oli <oliver.daniel.berg@gmail.com>
2018-08-31 11:40:09 +0200

    normed IDs done (also nested additives). Closes #1

--- util/normalizer/food_title_normalizer.py
+++ util/normalizer/food_title_normalizer.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import re
 
-DEFAULT_MEAL_CSV_PATH = '../../data/meal.csv'
+DEFAULT_MEAL_CSV_PATH = '../../data/meal_manually_cleaned.csv'
 
 class FoodNormalizer:
     """Normalizing the provided meal-csv-data into consise, usable format."""
@@ -18,7 +18,8 @@ class FoodNormalizer:
         self.meal_df_original = pd.read_csv(csv_os_file_path)
         self.meal_df = pd.read_csv(csv_os_file_path)
 
-    def separate_food_title(self, title):
+    @staticmethod
+    def separate_food_title(title):
         """Separate single string (food-title) into its food components."""
 
         re_title_delimiters = ', | mit | und |, dazu|:| in '
@@ -30,6 +31,14 @@ class FoodNormalizer:
             additives = additives.group(0) if additives else ''
             ad_escaped = additives.replace('(', '\(').replace(')','\)').replace('+','\+')
             component = re.sub(ad_escaped, '', component) if additives else component
+
+            addit_in_name_component = re.search('\([\w\+]{1,2}(,[\w\+]{1,2})*\)', component)    # in case of nested additives
+            addit_in_name_component = addit_in_name_component.group(0) if addit_in_name_component else ''
+            if addit_in_name_component:
+                escaped = addit_in_name_component.replace('(', '\(').replace(')','\)').replace('+','\+')
+                component = re.sub(escaped, '', component)
+                additives = additives[0:-1]+','+addit_in_name_component[1:]
+            
             component = component.strip()
             return [component, additives] if additives else [component]
         title_split_components = [separate_additives(component) for component in title_split]
@@ -38,7 +47,7 @@ class FoodNormalizer:
 
     def assign_norm_titles(self):
         titles = self.meal_df.title
-        titels_norm = [self.separate_food_title(title) for title in titles]
+        titels_norm = [FoodNormalizer.separate_food_title(title) for title in titles]
         titels_prim = [title[0][0] for title in titels_norm]
         self.meal_df = self.meal_df.assign(title_prim=titels_prim)
         self.meal_df = self.meal_df.assign(title_norm=titels_norm)
