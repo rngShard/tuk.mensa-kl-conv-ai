bd84f5a1fb7540739a185e93dd7fdf937112d7c3
Konstantin Kloster <konsti.kloster@gmail.com>
2018-09-02 19:04:31 +0200

    Funktion um Dateien aus Google Cloud Buckets herunterzuladen.

--- util/cloud_connection/bucket_connection.py
+++ util/cloud_connection/bucket_connection.py
@@ -20,3 +20,16 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     print('File {} uploaded to {}.'.format(
         source_file_name,
         destination_blob_name))
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
