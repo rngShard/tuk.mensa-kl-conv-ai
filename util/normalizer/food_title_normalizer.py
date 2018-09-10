e04e4b46cf6ba2f536378f6e3505868f4670211f
oli <oliver.daniel.berg@gmail.com>
2018-09-10 10:52:01 +0200

    uID assigned in normalizer class (based on exact simi of title_prim)

--- util/normalizer/food_title_normalizer.py
+++ util/normalizer/food_title_normalizer.py
@@ -23,6 +23,7 @@ class FoodNormalizer:
             self.meal_csv_path = csv_path
             self.meal_df_original = pd.read_csv(csv_path)
             self.meal_df = pd.read_csv(csv_path)
+        self.meal_index_tracker = set()
     @staticmethod
     def separate_food_title(title, ignore_additives):
         """Separate single string (food-title) into its food components."""
@@ -50,14 +51,47 @@ class FoodNormalizer:
             
         return title_split_components
 
+    def get_prim_title(self, titles_norm_comps):
+        """Takes components of title and generates a primary title of the meal.
+        
+        E.g. "[['Chinesisches Gemüse'], ['Hähnchenbruststreifen süß-sauer'], ['Reis']]" --> "chinesisches gemüse"
+        This takes into account the first meal component without additives and
+        - takes all lowercase
+        """
+        title_prim = titles_norm_comps[0][0]
+        title_prim = title_prim.lower()
+
+        return title_prim
+
+    def get_unique_meal_ids(self, titles_prim):
+        """Assign unique food IDs
+        
+        Based additionally on
+        - Levenshtein distance
+        - same-title dictionary
+        to filter for same food entries.
+        """
+        unique_titles = []
+        for title in titles_prim:
+            if title not in unique_titles:
+                unique_titles.append(title)
+        unique_meal_ids = [unique_titles.index(title) for title in titles_prim]
+        return unique_meal_ids
+
+
     def assign_norm_titles(self):
         titles = self.meal_df.title
-        titels_norm_additives = [FoodNormalizer.separate_food_title(title, True) for title in titles]
-        titels_norm = [FoodNormalizer.separate_food_title(title, False) for title in titles]
-        titels_prim = [title[0][0] for title in titels_norm]
-        self.meal_df = self.meal_df.assign(title_prim=titels_prim)
-        self.meal_df = self.meal_df.assign(title_norm=titels_norm)
-        self.meal_df = self.meal_df.assign(title_norm_additives=titels_norm_additives)
+
+        titles_norm_additives = [FoodNormalizer.separate_food_title(title, True) for title in titles]
+        titles_norm = [FoodNormalizer.separate_food_title(title, False) for title in titles]
+
+        titles_prim = [self.get_prim_title(title) for title in titles_norm]
+        titles_uID = self.get_unique_meal_ids(titles_prim)
+        
+        self.meal_df = self.meal_df.assign(title_prim=titles_prim)
+        self.meal_df = self.meal_df.assign(title_norm=titles_norm)
+        self.meal_df = self.meal_df.assign(title_norm_additives=titles_norm_additives)
+        self.meal_df = self.meal_df.assign(uID=titles_uID)
 
     def export_to_csv(self, path, absolute_path=None):
         if absolute_path is None:
@@ -77,14 +111,16 @@ if __name__ == '__main__':
     normalizer = FoodNormalizer(args.csv)
     normalizer.assign_norm_titles()
 
-    titles = normalizer.meal_df.loc[:,'title']
-    titles_norm = normalizer.meal_df.loc[:,'title_norm']
-    lens = [len(title_comps) for title_comps in titles_norm]
-    for i, l in enumerate(lens):
-        if l > 6:
-            # print(titles_norm[i])
-            print(titles[i])
-    # normalizer.export_to_csv('test_norm_meal.csv')
+    normalizer.export_to_csv('meal_norm_1.csv')
 
-    # print normalizer.meal_df.head()
+    # print(normalizer.meal_df.loc[0,'title_norm'])
+
+
+    # titles = normalizer.meal_df.loc[:,'title']
+    # titles_norm = normalizer.meal_df.loc[:,'title_norm']
+    # lens = [len(title_comps) for title_comps in titles_norm]
+    # for i, l in enumerate(lens):
+    #     if l > 6:
+    #         print(titles_norm[i])
+    #         # print(titles[i])
     
