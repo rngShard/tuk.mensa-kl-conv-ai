2c44e3d41a7177863f2bcb5f7d96652218a3619b
oli <oliver.daniel.berg@gmail.com>
2018-09-12 14:12:57 +0200

    WIP recommender user-on-all-food ratings refactoring

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -17,52 +17,67 @@ class Recommender:
             title_prim=[self.df_meals.loc[(self.df_meals['m_id'] == m_id),
                                           'uTitle'].to_string(index=False)
                         for m_id in self.df_ratings.loc[:, 'm_id']])
-        self.df_user_item = self.create_user_item()
-        ind = self.df_user_item.index
-        self.user_similarity = self.compute_user_sim()
-        self.user_similarity.columns = ind
-        self.user_similarity.index = ind
-    def create_user_item(self):
+        
+        self.df_user_item = self._create_user_item()
+        self.user_similarity = {}
+        for metric in ['correlation', 'cosine', 'dice', 'jaccard']:
+            self.user_similarity[metric] = self._create_user_user_sim(self.df_user_item.index, metric=metric)
+
+    def _create_user_item(self):
         df_user_item = self.df_ratings.pivot_table(index="user",
                                                    columns="m_id",
                                                    values="rating",
                                                    aggfunc=np.mean).fillna(0)
         return df_user_item
 
-    def compute_user_sim(self, metric="cosine"):
+    def _create_user_user_sim(self, user_index, metric):
         user_similarity = pd.DataFrame(1 - pairwise_distances(self.df_user_item, metric=metric))
+        user_similarity.index = user_index
+        user_similarity.columns = user_index
         return user_similarity
 
-    def get_all_similar_users(self, current_user):
-        similar_users = list(self.user_similarity[self.user_similarity.loc[current_user] > 0].index)
-        # similar_users = [i + 1 for i in similar_users]
+    def get_all_similar_users(self, current_user, metric="cosine"):
+        df_sim_mat = self.user_similarity[metric]
+        similar_users = list(df_sim_mat[df_sim_mat.loc[current_user] > 0].index)
+        similar_users.remove(current_user)
         return similar_users
 
-    def predict_rating(self, current_user, m_id):
-        similar_users = self.get_all_similar_users(current_user)
+    def predict_rating(self, current_user, m_id=None, metric='cosine', explain=False):
+        similar_users = self.get_all_similar_users(current_user, metric)
+
+        if explain:
+            print("Similar users: {}".format(similar_users))
+            if m_id is None:
+                print("Predicting ratings for user {} with `{}`".format(current_user, metric))
+            else:
+                print("Predicting rating for user {} & meal {} ({}) with `{}`".format(current_user, m_id, self.df_meals[self.df_meals['m_id'] == m_id].loc[:,'uTitle'].to_string(), metric))
+                print("... with sim. user ratings of {} for meal {}".format([self.df_user_item.loc[u,m_id] for u in similar_users], m_id))
+
+        if m_id is None:
+            return [self.predict(current_user, similar_users, m_id, metric) for m_id in self.df_user_item.columns]
+        else:
+            return self.predict(current_user, similar_users, m_id, metric)
+
+    def predict(self, current_user, similar_users, m_id, metric):
         real_rating = self.df_user_item.loc[current_user].loc[m_id]
         if real_rating != 0:
-            return m_id, real_rating
+            return real_rating
         else:
-            sum_ratings = 0
-            sum_sim = 0
+            sum_ratings, sum_sim = 0, 0
             for sim_user in similar_users:
-                if sim_user == current_user:
+                similarity = self.user_similarity[metric].loc[current_user].loc[sim_user]
+                rating = self.df_user_item.loc[sim_user].loc[m_id]
+                if rating == 0:
                     continue
-                else:
-                    similarity = self.user_similarity.loc[current_user].loc[sim_user]
-                    rating = self.df_user_item.loc[sim_user].loc[m_id]
-                    if rating == 0:
-                        continue
-                    sum_ratings += rating * similarity
-                    sum_sim += similarity
+                sum_ratings += rating * similarity
+                sum_sim += similarity
             if sum_sim == 0:
-                return m_id, 0
+                return 0
             else:
                 prediction = sum_ratings / sum_sim
-                return m_id, prediction
+                return prediction
 
 
 if __name__ == "__main__":
     recommender = Recommender()
-    print(recommender.predict_rating(1, 115))
+    print(recommender.predict_rating(1, metric='cosine', explain=True))
